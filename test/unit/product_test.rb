10b9ab779cdd98f05070e9deae27ddba141ee28f
Liu Jiyang <2411mail@gmail.com>
2012-11-24 15:57:52 +0800

    add store controller,css,functional test

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -13,8 +13,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do 
-    product = Product.new(:title => "programming ruby", :description => "great book", 
-                             :image_url => "/images/ruby.jpg")
+    product = Product.new(:title => "programming ruby", :description => "great book", :image_url => "/images/ruby.jpg")
     product.price = -1
     assert product.invalid?
     assert_equal "must be greater than or equal to 0.01",
@@ -40,7 +39,7 @@ class ProductTest < ActiveSupport::TestCase
   test "image url" do
     ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
             http://a.b.c/x/y/z/fred.gif }
-    bad = { fred.doc fred.gif/more fred.gif.more }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
@@ -55,17 +54,11 @@ class ProductTest < ActiveSupport::TestCase
   test "product title is invalid without a unique name" do 
     product = Product.new(:title => products(:ruby).title, 
                           :description => "great book", 
-                          :price => 49.40
+                          :price => 49.40,
                           :image_url => "/images/ruby.jpg")
     assert !product.save
     assert_equal "must be have a unique titile name",
       product.errors[:titile].join('; ')
   end
-
-
-
-
-
-
 
 end
